f32dc544528073127a38811b35923b85d7302865
Oleksiy Ostapenko <lelik4w@gmail.com>
2015-10-31 20:47:29 +0100

    plugin version update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://webtemplatemasters.com
  * Description: Advanced Features for Engorgio Theme
  * Author: ThemeMakers
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author URI: http://themeforest.net/user/ThemeMakers
  * Text Domain: tmm_theme_features
 */
