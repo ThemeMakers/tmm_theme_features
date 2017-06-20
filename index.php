ab65ae03cbe33b04a26f322972b73ea755b14fdd
Oleksiy Ostapenko <lelik4w@gmail.com>
2017-06-20 02:58:13 +0200

    update plugin version

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://webtemplatemasters.com
  * Description: Advanced Features for Cardealer Theme
  * Author: ThemeMakers
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author URI: http://themeforest.net/user/ThemeMakers
  * Text Domain: tmm_theme_features
  */
