8d15055b6c3910c4a1a364fcd33bc7e68896e4f8
Oleksiy Ostapenko <lelik4w@gmail.com>
2020-11-21 17:58:32 +0100

    update theme version

--- tmm-theme-features.php
+++ tmm-theme-features.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://webtemplatemasters.com
  * Description: Advanced Features for Accio Theme
  * Author: ThemeMakers
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author URI: http://themeforest.net/user/ThemeMakers
  * Text Domain: tmm_theme_features
 */
